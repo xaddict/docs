9576413edd0cb745d2b35ecba18631b807161c7d
Steffan Schiewe <steffan@schiewe.biz>
2013-01-21 18:44:46 +0100

    Updated Setup Joomla Menu

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 
-$dir = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'docs';
+define('ROOT', dirname(__FILE__) . DIRECTORY_SEPARATOR . 'docs');
 
 if (isset($_REQUEST['doc']) && !empty($_REQUEST['doc'])) {
 
-	$doc = $dir . '/' . $_REQUEST['doc'];
+	$doc = ROOT . '/' . $_REQUEST['doc'];
 
-	if (($fullpath = realpath($doc)) && file_exists($fullpath) && stripos($fullpath, $dir, 0) === 0) {
+	if (($fullpath = realpath($doc)) && file_exists($fullpath) && stripos($fullpath, ROOT, 0) === 0) {
 
 		echo '<link rel="stylesheet" href="assets/css/styles.css">';
 		echo '<div class="box box-text documentation">';
@@ -19,35 +19,42 @@ if (isset($_REQUEST['doc']) && !empty($_REQUEST['doc'])) {
 
 } else {
 
-	$iterator = new RecursiveDirectoryIterator($dir);
-
-	$renderer = function ($iterator) use (&$renderer, $dir) {
-		$html = array();
-		foreach ($iterator as $path) {
-			if ($path->isDir()) {
-				$html[] = "\n<li>";
-				$html[] = $path->getBaseName();
-				$html[] = '<ul>';
-				$html[] = $renderer(new RecursiveDirectoryIterator($path));
-				$html[] = '</ul>';
-				$html[] = '</li>';
-			} else {
-				$link = ltrim(preg_replace('/^'.preg_quote(str_replace(DIRECTORY_SEPARATOR, '/', $dir), '/').'/i', '', str_replace(DIRECTORY_SEPARATOR, '/', $path)), '/');
-				$html[] = "\n<li>";
-				$html[] = '<a href="index.php?doc='.urlencode($link).'">'.$path->getBaseName().'</a>';
-				$html[] = '</li>';
-			}
-		}
-		return implode("\n", $html);
-	}
-
 	?>
 
 	<h1>Documentation</h1>
 
 	<ul class="sections">
 
-		<?php echo $renderer($iterator); ?>
+		<?php 
+
+		function ls($path, &$html = array()) {
+
+			foreach (new FilesystemIterator($path, FilesystemIterator::SKIP_DOTS) as $file) {
+
+				if ($file->getFilename() == '.DS_Store') {
+					continue;
+				} elseif ($file->isDir()) {
+					$html[] = "\n<li>";
+					$html[] = $file->getBaseName();
+					$html[] = '<ul>';
+					$html[] = ls($file->getPathname());
+					$html[] = '</ul>';
+					$html[] = '</li>';
+				} else {
+					$link = ltrim(preg_replace('/^'.preg_quote(str_replace(DIRECTORY_SEPARATOR, '/', ROOT), '/').'/i', '', str_replace(DIRECTORY_SEPARATOR, '/', $file)), '/');
+					$html[] = "\n<li>";
+					$html[] = '<a href="index.php?doc='.urlencode($link).'">'.$file->getBaseName().'</a>';
+					$html[] = '</li>';
+				}
+
+			}
+
+			return implode("\n", $html);
+		}
+
+		echo ls(ROOT);
+
+		?>
 
 	</ul>
 
